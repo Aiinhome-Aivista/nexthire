4c74ec90f66d2ff32dc9f2c3159104c3737b986f
Sonia Khatun <sonia2khatun4@gmail.com>
2025-09-11 17:16:32 +0530

    google email recruiter login

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -59,7 +59,8 @@ $route['employer_dashboard'] = 'Recruiter_dashboard/index';
 $route['employer_job_post'] = 'Recruiter_job_post/index';
 $route['login/google_callback'] = 'Login/google_callback';
 $route['register/google_callback'] = 'Register/google_callback';
-
+$route['recruiter/google_callback'] = 'Recruiter/google_callback';
+$route['recruiter_login/google_callback'] = 'Recruiter_login/google_callback';
 $route['job_search'] = 'Jobsearch/index';
 $route['profile'] = 'Profile/index';
 $route['404_override'] = '';
